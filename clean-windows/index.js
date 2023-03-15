26095499645394a197f6c39c9ca5a573bb1991e5
jjq <jiangjiaqi1206@gmail.com>
2023-03-15 21:04:50 +0800

    windows

---
+++ clean-windows/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send(`Hello World!`);
+// });
+
+
+var PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log("My socket server is running");
+})
+// const server = app.listen(3000);
+
+const io = require('socket.io')(server);
+
+app.use(express.static('jjq'));
+
+io.sockets.on('connection',
+    // We are given a websocket object in our function
+    function (socket) {
+
+      console.log("We have a new client: " + socket.id);
+    })
